c5f374d9836f04910316989263819e7f033d7afa
alekztt <alexandros.theodoropoulos@nokia.com>
2018-08-19 22:43:29 +0300

    the reusable card component and passing components as Props

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 //functional component
 //presentation only
 
 const AlbumDetail = (props) => {
     return (
-        <View>
-            <Text>{props.record.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <Text>{props.record.title}</Text>
+            </CardSection>
+            <CardSection>
+                <Text>{props.record.artist}</Text>
+            </CardSection>
+            <CardSection>
+                
+            </CardSection>
+            
+        </Card>
     );
 };
 
